1a14a754e3b266db6e460397d2e31414c3c111cc
Amr Turki <amrturki79@gmail.com>
2026-04-03 09:44:21 +0200

    fix some bugs

--- lib/screens/login_page.dart
+++ lib/screens/login_page.dart
@@ -5,6 +5,7 @@ import 'package:chat_app/widget/custom_snack_bar.dart';
 import 'package:chat_app/widget/custom_text_field.dart';
 import 'package:firebase_auth/firebase_auth.dart';
 import 'package:flutter/material.dart';
+import 'package:modal_progress_hud_nsn/modal_progress_hud_nsn.dart';
 
 class LoginPage extends StatefulWidget {
   const LoginPage({super.key});
@@ -19,103 +20,129 @@ class _LoginPageState extends State<LoginPage> {
   GlobalKey<FormState> globalKey = GlobalKey();
   String? email;
   String? password;
+  bool isLoading = false;
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      backgroundColor: KPrimaryColor,
-      body: Padding(
-        padding: const EdgeInsets.symmetric(horizontal: 16.0),
-        child: Column(
-          children: [
-            Spacer(flex: 2),
-            Image(
-              height: 80,
-              width: 120,
-              image: AssetImage('assets/images/icon.png'),
-            ),
-            SizedBox(height: 14),
-            Text(
-              'Chat App',
-              style: TextStyle(fontSize: 18, fontWeight: FontWeight.bold),
-            ),
-            Spacer(flex: 1),
-            Row(
+    return ModalProgressHUD(
+      inAsyncCall: isLoading,
+      child: Scaffold(
+        backgroundColor: KPrimaryColor,
+        body: Padding(
+          padding: const EdgeInsets.symmetric(horizontal: 16.0),
+          child: Form(
+            key: globalKey,
+            child: Column(
               children: [
+                Spacer(flex: 2),
+                Image(
+                  height: 80,
+                  width: 120,
+                  image: AssetImage('assets/images/icon.png'),
+                ),
+                SizedBox(height: 14),
                 Text(
-                  'Sign in',
-                  style: TextStyle(fontSize: 24, fontWeight: FontWeight.bold),
+                  'Chat App',
+                  style: TextStyle(fontSize: 18, fontWeight: FontWeight.bold),
                 ),
-              ],
-            ),
-            SizedBox(height: 24),
-            CustomTextField(
-              text: 'Email',
-              onChange: (value) {
-                email = value;
-              },
-            ),
-            SizedBox(height: 24),
-            CustomTextField(
-              text: 'Password',
-              onChange: (value) {
-                password = value;
-              },
-            ),
-            SizedBox(height: 24),
-
-            CustomButton(
-              text: 'Sign in',
-              onTap: () async {
-                try {
-                  await SignUser();
-                } on FirebaseAuthException catch (e) {
-                  if (e.code == 'user-not-found') {
-                    CustomSnackBar(context, 'No user found for that email.');
-                  } else if (e.code == 'wrong-password') {
-                    CustomSnackBar(
-                      context,
-                      'Wrong password provided for that user.',
-                    );
-                  }
-                } catch (e) {
-                  CustomSnackBar(context, e.toString());
-                }
-              },
-            ),
-            SizedBox(height: 24),
-
-            Row(
-              mainAxisAlignment: MainAxisAlignment.center,
-              children: [
-                Text('do not have account? ', style: TextStyle(fontSize: 18)),
-                GestureDetector(
-                  onTap: () {
-                    Navigator.push(
-                      context,
-                      MaterialPageRoute(
-                        builder: (context) {
-                          return RegisterPage();
-                        },
+                Spacer(flex: 1),
+                Row(
+                  children: [
+                    Text(
+                      'Sign in',
+                      style: TextStyle(
+                        fontSize: 24,
+                        fontWeight: FontWeight.bold,
                       ),
-                    );
-                  },
-                  child: Text(
-                    'Sign up',
-                    style: TextStyle(fontSize: 18, fontWeight: FontWeight.bold),
-                  ),
+                    ),
+                  ],
                 ),
+                SizedBox(height: 24),
+                CustomTextField(
+                  text: 'Email',
+                  onChange: (value) {
+                    email = value;
+                  },
+                ),
+                SizedBox(height: 24),
+                CustomTextField(
+                  text: 'Password',
+                  onChange: (value) {
+                    password = value;
+                  },
+                ),
+                SizedBox(height: 24),
+
+                CustomButton(
+                  text: 'Sign in',
+                  onTap: () async {
+                    if (globalKey.currentState!.validate()) {
+                      isLoading = true;
+                      setState(() {});
+                      try {
+                        await SignUser();
+
+                        CustomSnackBar(context, 'Success');
+                      } on FirebaseAuthException catch (e) {
+                        if (e.code == 'user-not-found') {
+                          CustomSnackBar(
+                            context,
+                            'No user found for that email.',
+                          );
+                        } else if (e.code == 'wrong-password') {
+                          CustomSnackBar(
+                            context,
+                            'Wrong password provided for that user.',
+                          );
+                        }
+                      } catch (e) {
+                        CustomSnackBar(context, e.toString());
+                      }
+                      isLoading = false;
+                      setState(() {});
+                    }
+                  },
+                ),
+                SizedBox(height: 24),
+
+                Row(
+                  mainAxisAlignment: MainAxisAlignment.center,
+                  children: [
+                    Text(
+                      'do not have account? ',
+                      style: TextStyle(fontSize: 18),
+                    ),
+                    GestureDetector(
+                      onTap: () {
+                        Navigator.push(
+                          context,
+                          MaterialPageRoute(
+                            builder: (context) {
+                              return RegisterPage();
+                            },
+                          ),
+                        );
+                      },
+                      child: Text(
+                        'Sign up',
+                        style: TextStyle(
+                          fontSize: 18,
+                          fontWeight: FontWeight.bold,
+                        ),
+                      ),
+                    ),
+                  ],
+                ),
+                Spacer(flex: 3),
               ],
             ),
-            Spacer(flex: 3),
-          ],
+          ),
         ),
       ),
     );
   }
 
-Future<void> SignUser() async {
-  UserCredential userCredential = await FirebaseAuth.instance
-      .signInWithEmailAndPassword(email: email!, password: password!);
-}
-
+  Future<void> SignUser() async {
+    UserCredential userCredential = await FirebaseAuth.instance
+        .signInWithEmailAndPassword(email: email!, password: password!);
+  }
 }
--- lib/screens/register_page.dart
+++ lib/screens/register_page.dart
@@ -5,6 +5,7 @@ import 'package:chat_app/widget/custom_snack_bar.dart';
 import 'package:chat_app/widget/custom_text_field.dart';
 import 'package:firebase_auth/firebase_auth.dart';
 import 'package:flutter/material.dart';
+import 'package:modal_progress_hud_nsn/modal_progress_hud_nsn.dart';
 
 class RegisterPage extends StatefulWidget {
   const RegisterPage({super.key});
@@ -19,92 +20,112 @@ class _RegisterPageState extends State<RegisterPage> {
   GlobalKey<FormState> globalKey = GlobalKey();
   String? email;
   String? password;
+  bool isLoading = false;
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      backgroundColor: KPrimaryColor,
-      body: Padding(
-        padding: const EdgeInsets.symmetric(horizontal: 16.0),
-        child: Column(
-          children: [
-            Spacer(flex: 2),
-            Image(
-              height: 80,
-              width: 120,
-              image: AssetImage('assets/images/icon.png'),
-            ),
-            SizedBox(height: 14),
-            Text(
-              'Chat App',
-              style: TextStyle(fontSize: 18, fontWeight: FontWeight.bold),
-            ),
-            Spacer(flex: 1),
-            Row(
+    return ModalProgressHUD(
+      inAsyncCall: isLoading,
+      child: Scaffold(
+        backgroundColor: KPrimaryColor,
+        body: Padding(
+          padding: const EdgeInsets.symmetric(horizontal: 16.0),
+          child: Form(
+            key: globalKey,
+            child: Column(
               children: [
+                Spacer(flex: 2),
+                Image(
+                  height: 80,
+                  width: 120,
+                  image: AssetImage('assets/images/icon.png'),
+                ),
+                SizedBox(height: 14),
                 Text(
-                  'Sign up',
-                  style: TextStyle(fontSize: 24, fontWeight: FontWeight.bold),
+                  'Chat App',
+                  style: TextStyle(fontSize: 18, fontWeight: FontWeight.bold),
                 ),
-              ],
-            ),
-            SizedBox(height: 24),
-            CustomTextField(
-              text: 'Email',
-              onChange: (value) {
-                email = value;
-              },
-            ),
-            SizedBox(height: 24),
-            CustomTextField(
-              text: 'Password',
-              onChange: (value) {
-                password = value;
-              },
-            ),
-            SizedBox(height: 24),
-
-            CustomButton(
-              text: 'Sign up',
-              onTap: () async {
-                try {
-                  await RegisterUser();
-                } on FirebaseAuthException catch (e) {
-                  if (e.code == 'weak-password') {
-                    CustomSnackBar(
-                      context,
-                      'The password provided is too weak.',
-                    );
-                    print('');
-                  } else if (e.code == 'email-already-in-use') {
-                    CustomSnackBar(
-                      context,
-                      'The account already exists for that email.',
-                    );
-                  }
-                } catch (e) {
-                  CustomSnackBar(context, e.toString());
-                }
-              },
-            ),
-            SizedBox(height: 24),
-
-            Row(
-              mainAxisAlignment: MainAxisAlignment.center,
-              children: [
-                Text('do not have account? ', style: TextStyle(fontSize: 18)),
-                GestureDetector(
-                  onTap: () {
-                    Navigator.pop(context);
+                Spacer(flex: 1),
+                Row(
+                  children: [
+                    Text(
+                      'Sign up',
+                      style: TextStyle(fontSize: 24, fontWeight: FontWeight.bold),
+                    ),
+                  ],
+                ),
+                SizedBox(height: 24),
+                CustomTextField(
+                  text: 'Email',
+                  onChange: (value) {
+                    email = value;
                   },
-                  child: Text(
-                    'Sign in',
-                    style: TextStyle(fontSize: 18, fontWeight: FontWeight.bold),
-                  ),
                 ),
+                SizedBox(height: 24),
+                CustomTextField(
+                  text: 'Password',
+                  onChange: (value) {
+                    password = value;
+                  },
+                ),
+                SizedBox(height: 24),
+            
+                CustomButton(
+                  text: 'Sign up',
+                  onTap: () async {
+                    if (globalKey.currentState!.validate()) {
+                      isLoading = true;
+                      setState(() {});
+                      try {
+                        await RegisterUser();
+                        CustomSnackBar(context, 'Success');
+                      } on FirebaseAuthException catch (e) {
+                        if (e.code == 'weak-password') {
+                          CustomSnackBar(
+                            context,
+                            'The password provided is too weak.',
+                          );
+                          print('');
+                        } else if (e.code == 'email-already-in-use') {
+                          CustomSnackBar(
+                            context,
+                            'The account already exists for that email.',
+                          );
+                        }
+                      } catch (e) {
+                        CustomSnackBar(context, e.toString());
+                      }
+            
+                      isLoading = false;
+                      setState(() {
+                        
+                      });
+                    }
+                  },
+                ),
+                SizedBox(height: 24),
+            
+                Row(
+                  mainAxisAlignment: MainAxisAlignment.center,
+                  children: [
+                    Text('do not have account? ', style: TextStyle(fontSize: 18)),
+                    GestureDetector(
+                      onTap: () {
+                        Navigator.pop(context);
+                      },
+                      child: Text(
+                        'Sign in',
+                        style: TextStyle(
+                          fontSize: 18,
+                          fontWeight: FontWeight.bold,
+                        ),
+                      ),
+                    ),
+                  ],
+                ),
+                Spacer(flex: 3),
               ],
             ),
-            Spacer(flex: 3),
-          ],
+          ),
         ),
       ),
     );
