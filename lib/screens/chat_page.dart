2256518bdb0788ebae24633b297470be8facc586
Amr Turki <amrturki79@gmail.com>
2026-04-05 19:29:56 +0200

    create chat bubble

--- lib/screens/chat_page.dart
+++ lib/screens/chat_page.dart
@@ -1,4 +1,3 @@
-import 'package:chat_ss/constants.dart';
 import 'package:flutter/material.dart';
 
 class ChatPage extends StatelessWidget {
---
+++ lib/widget/chat_bubble.dart
@@ -0,0 +1,31 @@
+import 'package:flutter/material.dart';
+
+class ChatBubble extends StatelessWidget {
+  const ChatBubble({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Align(
+      alignment: Alignment.centerLeft,
+      child: Container(
+        height: 65,
+        width: 170,
+        padding: EdgeInsets.only(left: 16, right: 16, top: 16, bottom: 16),
+        alignment: Alignment.centerLeft,
+        margin: EdgeInsets.symmetric(horizontal: 16, vertical: 16),
+        decoration: BoxDecoration(
+          color: Colors.blue,
+          borderRadius: BorderRadius.only(
+            topLeft: Radius.circular(16),
+            topRight: Radius.circular(16),
+            bottomLeft: Radius.circular(16),
+          ),
+        ),
+        child: Text(
+          'How are you?',
+          style: TextStyle(fontSize: 14, color: Colors.white),
+        ),
+      ),
+    );
+  }
+}
