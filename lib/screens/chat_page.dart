2972d3310cfceb64f38b17bcfc53950cf85fec8d
Amr Turki <amrturki79@gmail.com>
2026-04-05 19:31:03 +0200

    finish chat page body

--- lib/screens/chat_page.dart
+++ lib/screens/chat_page.dart
@@ -1,3 +1,4 @@
+import 'package:chat_app/widget/chat_bubble.dart';
 import 'package:flutter/material.dart';
 
 class ChatPage extends StatelessWidget {
@@ -6,6 +7,33 @@ class ChatPage extends StatelessWidget {
   static String id = "ChatApp";
   @override
   Widget build(BuildContext context) {
-    return Scaffold(body: Container());
+    return Scaffold(
+      body: Column(
+        children: [
+          Expanded(
+            child: ListView.builder(
+              itemBuilder: (context, index) => ChatBubble(),
+            ),
+          ),
+
+          Padding(
+            padding: const EdgeInsets.symmetric(vertical: 16.0, horizontal: 14),
+            child: TextField(
+              onSubmitted: (value) {},
+              cursorColor: Colors.white,
+              decoration: InputDecoration(
+                hintText: 'Enter Your message\n',
+                suffixIcon: Icon(Icons.send),
+
+                border: OutlineInputBorder(
+                  borderRadius: BorderRadius.circular(16),
+                  borderSide: BorderSide(color: Colors.amber),
+                ),
+              ),
+            ),
+          ),
+        ],
+      ),
+    );
   }
 }
