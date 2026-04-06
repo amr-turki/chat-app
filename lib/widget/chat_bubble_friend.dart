40c9d320fdffbbb61e1aeef8e84d8361cd519af8
Amr Turki <amrturki79@gmail.com>
2026-04-06 17:15:26 +0200

    create chat bubble friend widget

---
+++ lib/widget/chat_bubble_friend.dart
@@ -0,0 +1,32 @@
+import 'package:chat_app/model/message_model.dart';
+import 'package:flutter/material.dart';
+
+class ChatBubbleFriend extends StatelessWidget {
+  const ChatBubbleFriend({super.key, required this.message});
+
+  final MessageModel message;
+  @override
+  Widget build(BuildContext context) {
+    return Align(
+      alignment: Alignment.centerLeft,
+      child: Container(
+        width: 170,
+        padding: EdgeInsets.only(left: 16, right: 16, top: 16, bottom: 16),
+        alignment: Alignment.centerLeft,
+        margin: EdgeInsets.symmetric(horizontal: 16, vertical: 16),
+        decoration: BoxDecoration(
+          color: Colors.blue,
+          borderRadius: BorderRadius.only(
+            topLeft: Radius.circular(16),
+            topRight: Radius.circular(16),
+            bottomRight: Radius.circular(16),
+          ),
+        ),
+        child: Text(
+          message.message,
+          style: TextStyle(fontSize: 14, color: Colors.white),
+        ),
+      ),
+    );
+  }
+}
